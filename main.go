37b7497bed0b8095b4d56d2f44e163c848aafda5
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-12-19 23:05:05 +0100

    Adds get location endpoint

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/julienschmidt/httprouter"
+	"github.com/levigross/grequests"
+)
+
+// GetLocation returns user physical location information
+func GetLocation(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	ipstackAPIKey := os.Getenv("IPSTACK_API_KEY")
+
+	resp, err := grequests.Get("http://api.ipstack.com/check?access_key="+ipstackAPIKey, nil)
+
+	if err != nil {
+		log.Fatalln("Unable to make request: ", err)
+	}
+
+	fmt.Println(resp.String())
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	router := httprouter.New()
+	router.GET("/get/location", GetLocation)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
